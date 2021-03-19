da9ffe760c8e569403a161b0086c9e7b5db9eb10
lrh2daizzo <herrera.lrh@gmail.com>
2021-03-19 16:53:26 -0500

    mounted + added users controller, views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
-class Users < ApplicationController
+class UsersController < ApplicationController
+    get '/users' do
+        erb :'users/show'
+    end
+
     get '/signup' do
         erb :'users/signup'
+    end
+
+    post '/signup' do
+        params.to_s 
+        redirect to '/users'
     end
 end
