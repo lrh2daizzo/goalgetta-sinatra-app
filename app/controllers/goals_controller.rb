7a861fcb493e39049ed02fafa096728195a66269
lrh2daizzo <herrera.lrh@gmail.com>
2021-03-31 19:16:49 -0500

    added delete action

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -34,4 +34,44 @@ class GoalsController < ApplicationController
         erb :'goals/show'
     end
 
+    get '/goals/:id/edit' do
+        @goal = Goal.find(params[:id])
+        erb :'goals/edit'
+    end
+
+    patch '/goals/:id' do
+        if logged_in?
+            if params[:goals][:title].empty?
+                redirect to "/goals/#{@goal.id}/edit"
+            else
+                    @goal = Goal.find(params[:id])
+                    if @goal && @goal.user == current_user
+                        if @goal.update(params[:goals])
+                            redirect to "/goals/#{@goal.id}"
+                        else
+                            redirect to "/goals/#{@goal.id}/edit"
+                        end
+                    else  
+                        redirect to '/goals'
+                    end
+            end
+        else
+            redirect to '/'
+        end
+    end
+
+    delete '/goals/:id/delete' do
+        if logged_in?
+            @goal = Goal.find_by_id(params[:id])
+            if @goal && @goal.user == current_user
+                @goal.delete 
+            end
+            redirect to "/goals"
+        else
+            redirect to '/login'
+        end
+    end
 end
+
+
+
