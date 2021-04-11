e3a6783be91339b024dc9d95beddc5f86eea85a8
lrh2daizzo <herrera.lrh@gmail.com>
2021-04-11 18:56:42 -0500

    edited logout method/route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     post '/login' do
         @user = User.find_by(:username => params[:username])
-        
+
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id 
             redirect to "/users/#{@user.id}"
@@ -39,8 +39,12 @@ class UsersController < ApplicationController
     end
 
     get '/users/:id' do
-        @user = User.find(params[:id])
-        erb :'users/show'
+        if logged_in?
+            @user = User.find(params[:id])
+            erb :'users/show'
+        else
+            redirect to '/'
+        end
     end
 
 
@@ -49,6 +53,15 @@ class UsersController < ApplicationController
             session.clear
             redirect to '/login'
         else 
+            redirect to '/'
+        end
+    end
+
+    get '/logout' do
+        if logged_in?
+            session.clear
+            redirect to '/login'
+        else
             redirect to '/'
         end
     end
